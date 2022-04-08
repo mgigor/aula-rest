d781e161bed3e9df95cb6623357095d394f6b35a
leonardo <leonardo.rs.2105@gmail.com>
2022-04-08 23:46:27 +0000

    .

---
+++ src/main/java/br/edu/unicesumar/aula/domain/produto.java
@@ -0,0 +1,25 @@
+package br.edu.unicesumar.aula.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class produto {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Double preco;
+
+    private String nome;
+}
